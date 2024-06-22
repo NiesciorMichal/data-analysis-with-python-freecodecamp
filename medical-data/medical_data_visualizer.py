96261d1aada342361d496f45c433aab60e6ebbea
Michał Nieścior <michalne95@gmail.com>
2024-06-22 19:42:19 +0000

    Medical Data

---
+++ medical-data/medical_data_visualizer.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 1
+df = None
+
+# 2
+df['overweight'] = None
+
+# 3
+
+
+# 4
+def draw_cat_plot():
+    # 5
+    df_cat = None
+
+
+    # 6
+    df_cat = None
+    
+
+    # 7
+
+
+
+    # 8
+    fig = None
+
+
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+
+# 10
+def draw_heat_map():
+    # 11
+    df_heat = None
+
+    # 12
+    corr = None
+
+    # 13
+    mask = None
+
+
+
+    # 14
+    fig, ax = None
+
+    # 15
+
+
+
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
